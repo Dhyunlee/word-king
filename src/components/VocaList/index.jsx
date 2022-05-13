e3ae4efb3fe73facbf69cd5d2a8f27541a6cefb6
Donghyun Lee <skyblue5030@naver.com>
2022-05-13 21:20:44 +0900

    Feat VocaList 컴포넌트 생성

---
+++ src/components/VocaList/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const VocaList = () => {
+  return (
+    <div className="vocaListContainer">
+      <ul className="vocaList">
+        <li></li>
+      </ul>
+    </div>
+  );
+};
+
+export default VocaList;
