68500e73237d1132fb500a9315ce9b04c822f4da
Donghyun Lee <skyblue5030@naver.com>
2022-05-26 00:19:51 +0900

    Design: WordContainer 스타일 적용

--- src/Template/defaultLayout.jsx
+++ src/Template/defaultLayout.jsx
@@ -4,10 +4,10 @@ import styled from '@emotion/styled';
 import Header from '../components/Header';
 const DefaultLayout = ({ children }) => {
   return (
-    <div>
+    <WordContainer>
       <Header />
       <MainContainer>{children}</MainContainer>
-    </div>
+    </WordContainer>
   );
 };
 
@@ -15,6 +15,11 @@ DefaultLayout.prototype = {
   children: prototypes.node.isRequired,
 };
 
+const WordContainer = styled.div`
+  width: 100%;
+  min-width: 500px;
+  height: auto;
+`;
 const MainContainer = styled.main`
   width: 80%;
   margin: 0 auto;
